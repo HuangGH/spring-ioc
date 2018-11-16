118f77319626e78a03d0f8a28db6fc72ad313dbe
HuangGH <HuangGH@users.noreply.github.com>
2018-11-16 16:40:50 +0800

    Update SpringTest.java

--- spring-ioc Maven Webapp/src/main/java/com/springioc/test/SpringTest.java
+++ spring-ioc Maven Webapp/src/main/java/com/springioc/test/SpringTest.java
@@ -8,5 +8,8 @@ public class SpringTest {
         MyClassPathXmlApplicationContext ctx = new MyClassPathXmlApplicationContext("beans.xml");
         Hello hello = (Hello)ctx.getBean("hello");
         hello.sayHello();
+        asdf
+            adsf
+            adsf;
     }
 }
