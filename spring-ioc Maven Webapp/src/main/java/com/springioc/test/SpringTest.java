89d07407bdcd2ce98e257927c6052246a94f0dee
Hgh <284858489@qq.com>
2018-11-16 17:09:05 +0800

    解决冲突了

--- spring-ioc Maven Webapp/src/main/java/com/springioc/test/SpringTest.java
+++ spring-ioc Maven Webapp/src/main/java/com/springioc/test/SpringTest.java
@@ -7,7 +7,7 @@ public class SpringTest {
     public static void main(String[] args) {
         MyClassPathXmlApplicationContext ctx = new MyClassPathXmlApplicationContext("beans.xml");
         Hello hello = (Hello)ctx.getBean("hello");
-        hello.sayHello();
-        hello.sayHello();
+        hello.sayHello();//测试冲突
+        hello.sayHello();//真的是冲突
     }
 }
